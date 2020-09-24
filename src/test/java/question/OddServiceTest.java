83d943439debac0d48601892bbe8406a9ce3ff3d
David Warn <davewarn@gmail.com>
2020-09-23 20:59:49 -0700

    version bump

--- src/test/java/question/OddServiceTest.java
+++ src/test/java/question/OddServiceTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class OddServiceTest {
 
+    /**
+     * Command to run test:
+     *     mvn -Dtest=OddServiceTest#testOddServiceTrue test
+     */
     @Test
     public void testOddServiceTrue() {
         assertEquals("test", "test");
