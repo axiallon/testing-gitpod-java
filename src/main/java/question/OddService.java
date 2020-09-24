27fafc9529bc4c6dbe28b8019cc12284f20d376b
David Warn <davewarn@gmail.com>
2020-09-23 20:09:33 -0700

    initial commit

---
+++ src/main/java/question/OddService.java
@@ -0,0 +1,7 @@
+package question;
+
+public class OddService {
+    
+    // TODO: implement body
+
+}
---
+++ src/test/java/question/OddServiceTest.java
@@ -0,0 +1,12 @@
+package question;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OddServiceTest {
+
+    @org.junit.jupiter.api.Test
+    public void testOddServiceTrue() {
+        assertEquals("test", "test");
+    }
+
+}
