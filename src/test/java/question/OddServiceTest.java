b988fb044f4f7cd3777e6293123b1bf0737d17bc
David Warn <davewarn@gmail.com>
2020-09-23 21:31:39 -0700

    add debug launch

--- src/test/java/question/OddServiceTest.java
+++ src/test/java/question/OddServiceTest.java
@@ -5,12 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class OddServiceTest {
 
+    public static void main(String[] args) {
+        OddServiceTest test = new OddServiceTest();
+        test.testOddServiceTrue();
+        test.testOddServiceFalse();
+    }
+
     /**
      * Command to run test:
      *     mvn -Dtest=OddServiceTest#testOddServiceTrue test
      */
     @Test
     public void testOddServiceTrue() {
+        assertEquals("test", "test");
+    }
+
+    /**
+     * Command to run test:
+     *     mvn -Dtest=OddServiceTest#testOddServiceFalse test
+     */
+    @Test
+    public void testOddServiceFalse() {
         assertEquals("test", "test");
     }
 
